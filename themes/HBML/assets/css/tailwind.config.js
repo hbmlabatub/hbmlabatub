717b0034cb875dfcee46b731032a7dc2be539698
Mason Lary <masonlary@gmail.com>
2023-05-23 23:00:50 -0400

    Fixed config file

--- themes/HBML/assets/css/tailwind.config.js
+++ themes/HBML/assets/css/tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  content: [
+    "./themes/**/layouts/**/*.html",
+    "./content/**/layouts/**/*.html",
+    "./layouts/**/*.html",
+    "./content/**/*.html"
+  ],
   theme: {
     extend: {}
   },
